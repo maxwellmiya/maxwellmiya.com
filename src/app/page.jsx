ef87f027c305404e55d373e26e9b71cca2037beb
Maxwell Miya <maxwell.miya@gmail.com>
2025-05-05 15:19:46 +0300

    Updated page sections and anlytics

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 "use client";
 import React from "react";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
