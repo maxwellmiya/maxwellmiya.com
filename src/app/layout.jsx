6b7cc996982128ae6a69bb52c9cf390bb3a4a5f7
Maxwell Miya <maxwell.miya@gmail.com>
2025-05-05 19:18:14 +0300

    Updates

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,12 @@
 import './globals.css';
-import { Analytics } from "@vercel/analytics/react";
-import { Analytics } from "@vercel/analytics/react";
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
         {children}
-        <SpeedInsights />
-        <Analytics/>
+        
       </body>
     </html>
   );
