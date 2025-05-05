4454ff3637d8dad07994bcdd2756148d37734aee
Maxwell Miya <maxwell.miya@gmail.com>
2025-05-05 19:10:55 +0300

    Updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Analytics } from "../../vercel/analytics/react";
-import { SpeedInsights } from "../../vercel/speed-insights/next";
+
 
 export default function Home() {
   // Add state for mobile menu
